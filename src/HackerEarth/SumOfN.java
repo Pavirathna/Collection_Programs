70d590305f3b2ce2f0518136f089e357aee47ef4
Raguauxo <ragu9921@gmail.com>
2019-07-19 18:30:40 +0530

    Edit Naming Conversion

--- src/HackerEarth/SumOfN.java
+++ src/HackerEarth/SumOfN.java
@@ -6,28 +6,28 @@ public class SumOfN {
 
     public static void main(String[] args) {
 
-        Scanner sc1 = new Scanner(System.in);
+        Scanner sc1 = new Scanner ( System.in );
 
-       // int num = sc1.nextInt();
-        int N= sc1.nextInt();
+        // int num = sc1.nextInt();
+        int num = sc1.nextInt ();
 
         // Define an array of integers of size N.
-        int[] numArray = new int[N];
+        int[] numArray = new int[num];
 
 
         int sum = 0;
-        for(int i=0; i<N; i++){
-            numArray[i] = sc1.nextInt(); // Get the input
+        for (int i = 0; i < num; i++) {
+            numArray[i] = sc1.nextInt (); // Get the input
         }
 
         // write the logic to add these numbers here:
-        for(int i=0;i<N;i++)
-             sum=sum+i;
+        for (int i = 0; i < num; i++)
+            sum = sum + i;
 
 
-            // Print the sum
-            System.out.print(sum);
-        sc1.close();
+        // Print the sum
+        System.out.print ( sum );
+        sc1.close ();
     }
 
 }
--- src/HackerEarth/SumOfNum2.java
+++ src/HackerEarth/SumOfNum2.java
@@ -6,27 +6,27 @@ public class SumOfNum2 {
 
     public static void main(String[] args) {
 
-        Scanner s = new Scanner(System.in);
+        Scanner s = new Scanner ( System.in );
 
-        int N = s.nextInt();
+        int num = s.nextInt ();
 
         // Define an array of integers of size N.
-        int[] numArray = new int[N];
+        int[] numArray = new int[num];
 
 
         int sum = 0;
-        for(int i=0; i<N; i++){
-            numArray[i] = s.nextInt(); // Get the input
+        for (int i = 0; i < num; i++) {
+            numArray[i] = s.nextInt (); // Get the input
         }
 
         // Write the logic to add these numbers here:
 
-        for(int i=0;i<N;i++) sum+=numArray[i]*numArray[i];
+        for (int i = 0; i < num; i++) sum += numArray[i] * numArray[i];
 
 
         // Print the sum
-        System.out.print(sum);
-        s.close();
+        System.out.print ( sum );
+        s.close ();
     }
 
 }
